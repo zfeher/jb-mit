b03bcb35fe0885b62327b32277e75ed0b9d1751d
Zoltan Feher <lostface@fastmail.fm>
2017-01-21 16:36:18 +0100

    Implement list box component

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as ComboBox } from './combo-box.vue';
 export { default as DatePicker } from './date-picker.vue';
+export { default as ListBox } from './list-box.vue';
---
+++ src/components/list-box.vue
@@ -0,0 +1,60 @@
+<template>
+  <div :id="`${id}-container`">
+    <label :for="id">{{ label }}</label>
+    <select :id="id" @change="handleChange($event)" :size="size" >
+      <option
+          v-for="option in options"
+          :value="option.value"
+          :selected="isSelected(option.value)" >
+        {{ option.text }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListBox',
+
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+
+    label: String,
+
+    size: {
+      type: Number,
+      default: 4,
+    },
+
+    selectedValue: {
+      type: String,
+      default: '',
+    },
+
+    options: {
+      type: Array,
+
+      default() {
+        return [];
+      },
+    },
+  },
+
+  methods: {
+    isSelected(value) {
+      return value === this.selectedValue;
+    },
+
+    handleChange(event) {
+      let selected = event.target.value;
+      this.$emit('select', selected);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
