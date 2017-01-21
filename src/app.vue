55dba10cce18b309bda2e4832ad8ba847fe9a8b7
Zoltan Feher <lostface@fastmail.fm>
2017-01-21 11:53:58 +0100

    Add origin and destination combo boxes with mocked data

--- src/app.vue
+++ src/app.vue
@@ -1,19 +1,92 @@
 <template>
   <div id="app">
-    <h1>{{ message }}</h1>
+    <ComboBox
+      id="origin"
+      @select="handleOriginSelect($event)"
+      label="Origin"
+      :options="originOptions"
+      :selectedValue="origin"
+    />
+
+    <ComboBox
+      id="destination"
+      @select="handleDestinationSelect($event)"
+      label="Destination"
+      :options="destOptions"
+      :selectedValue="destination"
+      :disabled="destDisabled"
+    />
   </div>
 </template>
 
 <script>
+import { ComboBox } from './components';
+
 export default {
-  name: 'app',
+  name: 'App',
+
+  components: {
+    ComboBox,
+  },
+
   data() {
     return {
-      message: 'Hello There'
+      origin: '',
+      destination: '',
     };
-  }
-};
+  },
 
+  computed: {
+    originOptions() {
+      return [{
+        text: '',
+        value: '',
+      }, {
+        text: 'orig1',
+        value: 'orig1',
+      }, {
+        text: 'orig2',
+        value: 'orig2',
+      }];
+    },
+
+    destOptions() {
+      // TODO FPish
+      if (this.origin) {
+        return [{
+          text: '',
+          value: '',
+        }, {
+          text: 'dest1',
+          value: 'dest1',
+        }, {
+          text: 'dest2',
+          value: 'dest2',
+        }];
+      } else {
+        return [{
+          text: '',
+          value: '',
+        }];
+      }
+    },
+
+    destDisabled() {
+      return !this.origin;
+    },
+  },
+
+  methods: {
+    handleOriginSelect(selected) {
+      this.origin = selected;
+      this.destination = '';
+    },
+
+    handleDestinationSelect(selected) {
+      this.destination = selected;
+    },
+  },
+};
 </script>
 
 <style>
