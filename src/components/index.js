0140e34db102701f7dfd58c958a3dad95228b1cf
Zoltan Feher <lostface@fastmail.fm>
2017-01-22 09:01:48 +0100

    Implement labell component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as ComboBox } from './combo-box.vue';
 export { default as DatePicker } from './date-picker.vue';
+export { default as Labell } from './labell.vue';
 export { default as ListBox } from './list-box.vue';
---
+++ src/components/labell.vue
@@ -0,0 +1,21 @@
+<template>
+  <span :id="id">{{text}}</span>
+</template>
+
+<script>
+export default {
+  name: 'Labell',
+
+  props: {
+    id: String,
+
+    text: {
+      type: String,
+      required: true,
+    }
+  },
+};
+</script>
+
+<style>
+</style>
