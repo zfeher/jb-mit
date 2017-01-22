ec9fcb4a633d7e8a48f00f31b306502140f2ad55
Zoltan Feher <lostface@fastmail.fm>
2017-01-22 11:36:18 +0100

    Implement error message component

---
+++ src/components/error-message.vue
@@ -0,0 +1,20 @@
+<template>
+  <p :style="{ boxSizing: 'border-box', margin: 0, padding: 0, display: 'inline-block', color: 'orange' }" >
+    {{ message }}
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'ErrorMessage',
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as ComboBox } from './combo-box.vue';
 export { default as DatePicker } from './date-picker.vue';
+export { default as ErrorMessage } from './error-message.vue';
 export { default as Labell } from './labell.vue';
 export { default as ListBox } from './list-box.vue';
