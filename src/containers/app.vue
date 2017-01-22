cf4acf0f6b6cdc0cd50c17039ba0000f6931797a
Zoltan Feher <lostface@fastmail.fm>
2017-01-22 10:10:45 +0100

    Extract search result panel component from app

--- src/containers/app.vue
+++ src/containers/app.vue
@@ -1,76 +1,26 @@
 <template>
   <div id="app">
     <SearchPanel id="search-panel" />
-
-    <Labell text="Departure Flights" />
-    <ListBox
-      id="departureFlight"
-      @select="handleDepFlightSelect($event)"
-      :options="depFlightOptions"
-      :selectedValue="departureFlight"
-    />
-
-    <Labell text="Return Flights" />
-    <ListBox
-      id="returnFlight"
-      @select="handleRetFlightSelect($event)"
-      :options="retFlightOptions"
-      :selectedValue="returnFlight"
-    />
-
+    <SearchResultPanel id="search-result-panel" />
   </div>
 </template>
 
 <script>
-import { Labell, ListBox } from '../components';
 import SearchPanel from './search-panel.vue';
+import SearchResultPanel from './search-result-panel.vue';
 
 export default {
   name: 'App',
 
   components: {
-    Labell,
-    ListBox,
     SearchPanel,
-  },
-
-  data() {
-    return {
-      departureFlight: '',
-      returnFlight: '',
-    };
+    SearchResultPanel,
   },
 
   computed: {
-    depFlightOptions() {
-      return [{
-        text: 'dep1',
-        value: 'dep1',
-      }, {
-        text: 'dep2',
-        value: 'dep2',
-      }];
-    },
-
-    retFlightOptions() {
-      return [{
-        text: 'ret1',
-        value: 'ret1',
-      }, {
-        text: 'ret2',
-        value: 'ret2',
-      }];
-    },
   },
 
   methods: {
-    handleDepFlightSelect(selected) {
-      this.departureFlight = selected;
-    },
-
-    handleRetFlightSelect(selected) {
-      this.returnFlight = selected;
-    },
   },
 };
 </script>
---
+++ src/containers/search-result-panel.vue
@@ -0,0 +1,81 @@
+<template>
+  <div :id="id" >
+    <Labell text="Departure Flights" />
+    <ListBox
+      id="departureFlight"
+      @select="handleDepFlightSelect($event)"
+      :options="depFlightOptions"
+      :selectedValue="departureFlight"
+    />
+
+    <Labell text="Return Flights" />
+    <ListBox
+      id="returnFlight"
+      @select="handleRetFlightSelect($event)"
+      :options="retFlightOptions"
+      :selectedValue="returnFlight"
+    />
+  </div>
+</template>
+
+<script>
+import { Labell, ListBox } from '../components';
+
+export default {
+  name: 'SearchResultPanel',
+
+  props: {
+    id: {
+      type: String,
+      required: true,
+    }
+  },
+
+  components: {
+    Labell,
+    ListBox,
+  },
+
+  data() {
+    return {
+      departureFlight: '',
+      returnFlight: '',
+    };
+  },
+
+  computed: {
+    depFlightOptions() {
+      return [{
+        text: 'dep1',
+        value: 'dep1',
+      }, {
+        text: 'dep2',
+        value: 'dep2',
+      }];
+    },
+
+    retFlightOptions() {
+      return [{
+        text: 'ret1',
+        value: 'ret1',
+      }, {
+        text: 'ret2',
+        value: 'ret2',
+      }];
+    },
+  },
+
+  methods: {
+    handleDepFlightSelect(selected) {
+      this.departureFlight = selected;
+    },
+
+    handleRetFlightSelect(selected) {
+      this.returnFlight = selected;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
