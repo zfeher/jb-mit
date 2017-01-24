17d494a6b8eaa2c15964ca5183b4c2b069c3ab3c
Zoltan Feher <lostface@fastmail.fm>
2017-01-24 14:20:20 +0100

    Inject store in main

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue';
 import { App } from './containers';
+import { createStore } from './store';
+
+let store = createStore();
 
 new Vue({
   el: '#app',
+  store,
   render: h => h(App),
 });
